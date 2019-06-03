8c61b87dde0d23df09bf3fa6b7e039c1d01af2d4
lidertic <lidertic@gmail.com>
2019-06-03 15:59:45 +0200

    Canvis i deployment a firebase

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ Intentem crear un contenidor per aplicacions que faci de portal. Pugui compartir
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
+Run `ng build --base-href . --prod`
+
 ## Code scaffolding
 
 Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,66 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './auth.guard';
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     redirectTo: 'home',
+//     pathMatch: 'full'
+//   },
+//   {
+//     path: 'home',
+//     component: HomeComponent
+//   },
+//   {
+//     path: 'app1',
+//     loadChildren: () =>
+//       import('./lazy/app1-wrapper.module').then(m => m.App1WrapperModule)
+//   },
+//   {
+//     path: 'app2',
+//     loadChildren: () =>
+//       import('./lazy/app2-wrapper.module').then(m => m.App2WrapperModule)
+//   },
+//   {
+//     path: 'app1/**',
+//     redirectTo: 'app1'
+//   },
+//   {
+//     path: 'app2/**',
+//     redirectTo: 'app2'
+//   },
+//   {
+//     path: '**',
+//     redirectTo: 'home'
+//   }
+// ];
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
   {
     path: 'home',
     component: HomeComponent
   },
-  // {
-  //   path: 'app1',
-  //   loadChildren: './lazy/app1-wrapper.module#App1WrapperModule'
-  // },
   {
     path: 'app1',
     loadChildren: () =>
-      import('./lazy/app1-wrapper.module').then(m => m.App1WrapperModule)
+      import('../../projects/app1/src/app/app.module').then(
+        mod => mod.AppModule
+      ),
+    canLoad: [AuthGuard]
   },
   {
     path: 'app2',
     loadChildren: () =>
-      import('./lazy/app2-wrapper.module').then(m => m.App2WrapperModule)
+      import('../../projects/app2/src/app/app.module').then(
+        mod => mod.AppModule
+      )
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
   },
   {
     path: '**',
@@ -33,7 +69,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
